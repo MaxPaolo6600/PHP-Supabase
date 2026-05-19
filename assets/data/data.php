053dbd9f8ee1172f24c881836489c614f0e57f00
MaxPaolo6600 <paolomax993@gmail.com>
2026-05-19 19:53:23 -0300

    kvbjh

---
+++ assets/data/data.php
@@ -0,0 +1,18 @@
+<?php
+
+$url = "https://dhocokkmmaffhzzaawuj.supabase.co/rest/v1/posts";
+
+$key = "sb_publishable_GI6YJxAvY2sj1KrCIqxU9w_6AJZI8Xa";
+
+$options = [
+    "http" => [
+        "header" =>
+        "apikey: $key\r\n" .
+            "Authorization: Bearer $key\r\n" .
+            "Content-Type: application/json\r\n"
+    ]
+];
+$context = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+$data = json_decode($response, true);
+?>
--- index.php
+++ index.php
@@ -1,56 +1,35 @@
 <?php
 
-$url = "https://dhocokkmmaffhzzaawuj.supabase.co/rest/v1/posts";
+include("./assets/data/data.php");
 
-$key = "sb_publishable_GI6YJxAvY2sj1KrCIqxU9w_6AJZI8Xa";
-
-$options = [
-    "http" => [
-        "header" =>
-        "apikey: $key\r\n" .
-            "Authorization: Bearer $key\r\n" .
-            "Content-Type: application/json\r\n"
-    ]
-];
-$context = stream_context_create($options);
-$response = file_get_contents($url, false, $context);
-$data = json_decode($response, true);
 ?>
-
 <!DOCTYPE html>
-<html lang="pt-BR">
-
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Posts do Supabase</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            padding: 40px;
-            background: #f5f5f5;
-        }
-
-        .card {
-            background: white;
-            padding: 20px;
-            border-radius: 10px;
-            margin-bottom: 15px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Max´s Store</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
-
 <body>
-    <h1>Posts</h1>
-    <?php foreach ($data as $post): ?>
-        <div class="card">
-            <h3>ID: <?= $post['id'] ?></h3>
-            <p>
-                Título:
-                <?= $post['titulo_post'] ?>
-            </p>
+    <header>
+        <div class="header_opcao">
+            <a href="#">opcao1</a>
+            <a href="#">opcao1</a>
+            <a href="#">opcao1</a>
         </div>
-    <?php endforeach; ?>
-</body>
+        <div class="header_nome">
+            <h3>Max´s Store</h3>
+        </div>
+    </header>
+    <main>
+        <section>
+            
+            <img src="./assets/img/o.jpg" alt="zelda totk">
+        </section>
+    </main>
+    <footer>
 
+    </footer>
+</body>
 </html>
