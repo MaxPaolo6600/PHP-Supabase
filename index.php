e671fc06203d96f77e502656d6bbadecebceb6b0
MaxPaolo6600 <paolomax993@gmail.com>
2026-05-19 21:45:59 -0300

    jkhlfvjyh

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ include("./assets/data/data.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Max´s Store</title>
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
+
 <body>
     <header>
         <div class="header_opcao">
@@ -24,12 +26,26 @@ include("./assets/data/data.php");
     </header>
     <main>
         <section>
-            
-            <img src="./assets/img/o.jpg" alt="zelda totk">
+            <form method="POST">
+                <input type="text" id="numero" name="numero" require>
+                <button type="submit">Enviar numero</button>
+            </form>
+            <?php
+                $num = 0;
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $num = $_POST["numero"];
+                }
+                for ($i = 1; $i <= 10; $i++) {
+                        $res = $i * $num;
+                        echo "$num X $i = $res";
+                        echo "</br>";
+                    }
+                ?>
         </section>
     </main>
     <footer>
 
     </footer>
 </body>
+
 </html>
